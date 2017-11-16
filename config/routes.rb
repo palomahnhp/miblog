783dfbe255095962f2e085e836696ee667108788
palomahnhp <hernandezp@madrid.es>
2017-11-16 12:17:14 +0100

    create articulos

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'estaticas/home'
-
-  get 'estaticas/index'
-
   get 'home/index'
   root 'home#index'
+
+  resources :articulos
+  get 'estaticas/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
